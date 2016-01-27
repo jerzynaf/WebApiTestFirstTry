316296cfc750f69ce75cea6dea9ea6b193c4868d
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-27 12:10:53 +0000

    Initial structure + app.js and person entity implemented.

---
+++ MvcTest/MvcTest.Web/Assets/js/entities/person.js
@@ -0,0 +1,68 @@
+﻿MyApp.module("Entities", function (Entities, PeopleManager, Backbone, Marionette, $, _) {
+  Entities.Person = Backbone.Model.extend({
+    urlRoot: "api/PeopleApi",
+    defaults: {
+      firstName: '',
+      lastName: ''
+    }
+  });
+
+  Entities.PeopleCollection = Backbone.Collection.extend({
+    url: "api/PeopleApi",
+    model: Entities.Person
+  });
+
+  Entities.ColourItemModel = Backbone.Model.extend({
+  });
+
+  Entities.ColourCollectionModel = Backbone.Collection.extend({
+    model: Entities.ColourItemModel
+  });
+
+  var API = {
+    getPeopleEntities: function () {
+      var people = new Entities.PeopleCollection();
+      var defer = $.Deferred();
+      people.fetch({
+        success: function (data) {
+          defer.resolve(data);
+        },
+        error: function () {
+          defer.resolve(undefined);
+        }
+      });
+      var promise = defer.promise();
+      return promise;
+    },
+    getPersonEntity: function (personId) {
+      var person = new Entities.Person({id: personId});
+      var defer = $.Deferred();
+      person.fetch({
+        success: function (data) {
+          defer.resolve(data);
+        },
+        error: function () {
+          defer.resolve(undefined);
+        }
+      });
+      var promise = defer.promise();
+      return promise;
+    },
+    getColourEntities: function(rawColours) {
+      var colours = new Entities.ColourCollectionModel(rawColours);
+      return colours;
+    }
+  };
+
+  PeopleManager.reqres.setHandler("person:entities", function(){
+     return API.getContactEntities();
+  });
+
+  PeopleManager.reqres.setHandler("person:entity", function (personId) {
+    return API.getContactEntity(personId);
+  });
+
+  PeopleManager.reqres.setHandler("colour:entities", function (rawColours) {
+    return API.getColourEntities();
+  });
+});

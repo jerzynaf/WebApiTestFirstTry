ba5cea847908d557435dfd2859fce48057f05081
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-27 12:40:15 +0000

    people_app.js implemented

---
+++ MvcTest/MvcTest.Web/Assets/js/apps/people/people_app.js
@@ -0,0 +1,35 @@
+﻿MyApp.module("PeopleApp", function (PeopleApp, PeopleManager, Backbone, Marionette, $, _) {
+
+  PeopleApp.Router = Marionette.AppRouter.extend({
+    appRoutes: {
+      "people": "listPeople",
+      "people/edit/:id": "editPerson"
+    }
+
+  });
+
+  var API = {
+    listPeople: function () {
+      //execute controller's action to list people
+    },
+    editPerson: function (id) {
+      //execute controller's action to edit person
+    }
+  };
+
+  PeopleManager.on("people:list", function () {
+    PeopleManager.navigate("people");
+    API.listPeople();
+  });
+
+  PeopleManager.on("person:edit", function (id) {
+    PeopleManager.navigate("people/edit/" + id);
+    API.editPerson(id);
+  });
+
+  PeopleManager.addInitializer(function() {
+    new PeopleApp.Router({
+      controller: API
+  });
+  });
+});
